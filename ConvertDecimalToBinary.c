427f5ceff1156c0409141f83ca7c316a1766fd2c
PalakShrivastava <shrivastavapalak9@gmail.com>
2025-05-07 02:30:33 +0530

    Update ConvertDecimalToBinary.c

--- ConvertDecimalToBinary.c
+++ ConvertDecimalToBinary.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int num, temp = 0, Remainder, Quotient, i = 0, j=0;
+    int num, temp = 0, Remainder, Quotient, i = 0, j=0, count0=0, count1=0;
     int a[10];
     printf("Enter Decimal Number\n");
     scanf("%d", &num);
@@ -11,6 +11,12 @@ int main()
        Remainder = temp%2;
        Quotient = temp/2;
        a[i] = Remainder;
+        if(Remainder == 1)
+        {
+            count1++;
+        }
+        else
+        count0++;
        i++;
        temp = Quotient;
     }
